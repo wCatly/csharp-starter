12b6f352cad92d02832541928d246110c34649ba
wCat <36376486+wCatly@users.noreply.github.com>
2022-08-23 17:33:55 +0300

    Communit

---
+++ Login.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoUpdaterDotNET;
+using Discord.Net;
+using SharpConfig;
+
+namespace RiotAutomatization
+{
+    public partial class Login : Form
+    {
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void siticoneControlBox1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
